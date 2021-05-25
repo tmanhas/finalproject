d9aa31e9be9fe8452a10ed7c94d330b304f360ba
Roberto <roberto.infante03@gmail.com>
2021-05-24 23:08:34 -0700

    Add R file

---
+++ R/data_analysis.R
@@ -0,0 +1,16 @@
+
+
+nba_data <- read.csv("nba_games_stats.csv")
+
+## Get average of points during the season (Probably group by year then avg)
+
+## Get average of points allowed during the season
+
+## Find the best defensive team per season
+
+## Find best offensive team per season
+
+## Graph on how the total defensive attributes (Rebounds, steals, TO, etc) are compared to other teams
+## This will answer our hypothesis of being a defensice team makes you win the championship
+## If hypothesis is correct, graph other attributes
+## If hypothesis is incorrect, look for other clues on what makes a champion

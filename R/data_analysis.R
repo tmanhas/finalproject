2070e4f8ce8705a329b8480a19f6cd435452cffc
Roberto <roberto.infante03@gmail.com>
2021-05-26 00:19:47 -0700

    Create data structures for analysis

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -1,10 +1,65 @@
-
+library("dplyr")
+library("tidyverse")
+library("maps")
+library("stringr")
 
 nba_data <- read.csv("nba_games_stats.csv")
 
-## Get average of points during the season (Probably group by year then avg)
+nba_data_season <- mutate(nba_data, DateCorrectYear = str)
 
-## Get average of points allowed during the season
+nba_data_season <- mutate(nba_data, SeasonYear = as.Date(Date, "%m/%d/%y"))
+
+nba_data_season_14_15 <- filter(nba_data_season, SeasonYear <= "2015-04-15" )
+
+nba_data_season_15_16 <- filter(nba_data_season, SeasonYear <= "2016-06-19") %>%
+  filter(SeasonYear > "2015-04-15")
+
+nba_data_season_16_17 <- filter(nba_data_season, SeasonYear <= "2017-06-19") %>%
+  filter(SeasonYear > "2016-06-19")
+
+nba_data_season_17_18 <- filter(nba_data_season, SeasonYear <= "2018-06-19") %>%
+  filter(SeasonYear > "2017-06-19")
+
+
+
+## Get average of points during the season 
+
+avg_14_15 <- summarise(nba_data_season_14_15, AvgPoints = mean(TeamPoints), MedianPoints = median(TeamPoints))
+
+avg_15_16 <- summarise(nba_data_season_15_16, AvgPoints = mean(TeamPoints), MedianPoints = median(TeamPoints))
+
+avg_16_17 <- summarise(nba_data_season_16_17, AvgPoints = mean(TeamPoints), MedianPoints = median(TeamPoints))
+
+avg_17_18 <- summarise(nba_data_season_17_18, AvgPoints = mean(TeamPoints), MedianPoints = median(TeamPoints))
+
+## Get average of points during the season per team
+
+avg_per_team_14_15 <- group_by(nba_data_season_14_15, Team) %>%
+  summarise(AvgTeam = mean(TeamPoints), MedTeam = median(TeamPoints))
+
+avg_per_team_15_16 <- group_by(nba_data_season_15_16, Team) %>%
+  summarise(AvgTeam = mean(TeamPoints), MedTeam = median(TeamPoints))
+
+avg_per_team_16_17 <- group_by(nba_data_season_16_17, Team) %>%
+  summarise(AvgTeam = mean(TeamPoints), MedTeam = median(TeamPoints))
+
+avg_per_team_17_18 <- group_by(nba_data_season_17_18, Team) %>%
+  summarise(AvgTeam = mean(TeamPoints), MedTeam = median(TeamPoints))
+
+## Get average of points allowed during the season per team
+
+avg_pAgainst_team_14_15 <- group_by(nba_data_season_14_15, Team) %>%
+  summarise(AvgAgainstTeam = mean(OpponentPoints), MedAgainstTeam = median(OpponentPoints))
+
+avg_pAgainst_team_15_16 <- group_by(nba_data_season_15_16, Team) %>%
+  summarise(AvgAgainstTeam = mean(OpponentPoints), MedAgainstTeam = median(OpponentPoints))
+
+avg_pAgainst_team_16_17 <- group_by(nba_data_season_16_17, Team) %>%
+  summarise(AvgAgainstTeam = mean(OpponentPoints), MedAgainstTeam = median(OpponentPoints))
+
+avg_pAgainst_team_17_18 <- group_by(nba_data_season_17_18, Team) %>%
+  summarise(AvgAgainstTeam = mean(OpponentPoints), MedAgainstTeam = median(OpponentPoints))
+
 
 ## Find the best defensive team per season
 

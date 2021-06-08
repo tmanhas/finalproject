ba7dc8b94a101921587d9b1b633638c1a3794bf9
Tara Manhas <tmanhas@uw.edu>
2021-06-07 18:21:14 -0700

    added intro and summary

--- NBA-Analysis/ui.R
+++ NBA-Analysis/ui.R
@@ -21,6 +21,36 @@ fluidPage(theme = shinytheme("superhero"),
           tabsetPanel(type = "tabs",
                       tabPanel("Introduction",
                                h2("Introduction"),
+                               p("Our Data looks at the Analysis of the NBA Games between the years 
+                               2014-2018, on a season basis. We have used a number of statistics to 
+                               analyse the game play of each and every team at the NBA games between 
+                               2014-2018. The major one we have used to understand the probability of a 
+                               team winning is called 'novelPlusMinus', and this helps us in understanding 
+                               where a team that won or was close to winning might have gone wrong/right. 
+                               This data base is for NBA lovers, and we have made a lot analysis through 
+                               data science to make users understand the teams better and have them 
+                               represented through numerous statistics. Our data comes Kaggle, and a 
+                               lot of data extraction has been done from the original data, which is 
+                               being represented through graphs as you will see in the next couple of tabs. 
+                               These graphs will enable users to compare the different NBA teams, and not just 
+                               compare with other teams, but also see how the trajectory of each team has changed 
+                               over time in the complete NBA data table."),
+                               br(),
+                               h2("Summary"),
+                               p("We chose to work with an NBA statistics database because we think that it is a sport that can be overall predictable by looking at the team statistics from the entire season and by using descriptive statistics which helps us find a number, 
+          that we call the 'novelPlusMinus' statistics. This predicts which teams are going to reach the final that year and who is the most probable to win. 
+          First we needed to do some data wrangling to allow us to manage the data properly and find the statistics that were going to contribute to create our Novel Statistic. 
+          In the meantime we spent some time reading articles to understand what basketball players, coaches, and analysts had to say about all the attributes that make a great basketball team; 
+          and we end up with a hypothesis that rebounding was a key part in the success of Micheal Jordan’s Bulls team, and critical for Miami to reach the finals last year. 
+             Is this the only stat that matters? 
+          Of course not, but in our data science part of the analysis, we weighted this statistic more than some of the other stats (Assists, steals, blocks, 3pt shooting, etc). 
+          We made an exploratory data analysis to see the distribution of all the features that happen in a basketball game and depending on the mean and median.
+          We then filtered the teams in different “buckets” of information to categorize the statistics since we do not want to be exact, as basketball is a sport with multiple probability collisions. 
+          In the end, we got two statistic numbers, one for offensive and another defensive in which we add them together and end up with a single Novel Plus minus statistic. 
+          We proved these values to see if we could predict which team won in different years from 2014 to 2018  and we got good results as we got 3 out of 4, which proved our point. And in the one that we didn’t get right, the team that was the champion came second place. 
+          With this, our hypothesis of weighting rebounds makes more sense if we add them to all the statistics that happen in a basketball game. We are optimistic in saying that we can also suggest an NBA team on what type of game or players they need in order to enhance their game in relation to one of the seasons. 
+          For example, if a team has a novel statistic of 4.4 and the champions had a 5.3, we could tell analyze where the team lacked or had lapses in and what different types of features they should optimize in order to have that 5.3.
+")
                                ),
                       tabPanel("Defensive Statistics",
                               h2("Defensive Stats"),
